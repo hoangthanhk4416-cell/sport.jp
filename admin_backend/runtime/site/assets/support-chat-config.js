d61ee9482352657c08fe8be0c11d9feed5d005eb
hoangthanhk4416-cell <hoangthanhk4416@gmail.com>
2026-08-16 10:06:07 -0400

    Exclude generated admin runtime

--- admin_backend/runtime/site/assets/support-chat-config.js
+++
@@ -1,7 +0,0 @@
-window.TEAMSPIRIT_SUPPORT_CONFIG = Object.assign({
-  aiEndpoint: "https://script.google.com/macros/s/AKfycbwXQR3Tqy2gtsxINHZ36WZvXsBiVRTWpQoP9tzgFe_p3dR-S01dk-8SEZKd9n9t0zcSGg/exec",
-  aiEnabled: true,
-  maxAiRequestsPerDay: 10,
-  lineUrl: "https://lin.ee/qE1TJJ5",
-  instagramUrl: "https://www.instagram.com/teamspirit.jp/"
-}, window.TEAMSPIRIT_SUPPORT_CONFIG || {});
